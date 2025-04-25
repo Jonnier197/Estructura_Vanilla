cc7993541544dd2e2026f91f60d90243f5a0c742
Jonnier197 <jonnier.cadena01@unicatolica.edu.co>
2025-04-25 10:39:08 -0500

    Actualización

--- Assets/scrips.js
+++ Assets/scrips.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function(){
     const downloadBtn = document.getElementById("downloadBtn");
     if(downloadBtn){
         downloadBtn.addEventListener("click", function(){
-            const element = document.body
+            const element = document.body;
             const opt = {
                 margin: 0.5,
-                filename: "Mi CV.pdf",
+                filename: "Mi_CV.pdf",
                 image:{type: "jpeg", quality: 0.98},
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: "in", format: "letter", orientation: "portrair"},
